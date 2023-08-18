ed95d47496221a0e3251d7c9039b09325ce1a9f2
Dominik <dominiks8318@gmail.com>
2023-08-18 16:08:11 +0200

    Added UpdateService. Configuration improvements.

--- src/main/java/com/github/imdmk/automessage/configuration/PluginConfiguration.java
+++ src/main/java/com/github/imdmk/automessage/configuration/PluginConfiguration.java
@@ -2,15 +2,20 @@ package com.github.imdmk.automessage.configuration;
 
 import com.github.imdmk.automessage.notification.configuration.NotificationConfiguration;
 import eu.okaeri.configs.OkaeriConfig;
+import eu.okaeri.configs.annotation.Comment;
 
 public class PluginConfiguration extends OkaeriConfig {
 
-    public boolean checkForUpdates = true;
+    @Comment("# Check if you are using the latest version when enabling the plugin?")
+    public boolean checkForUpdate = true;
 
+    @Comment("# Auto message command enabled?")
     public boolean autoMessageCommandEnabled = true;
 
+    @Comment("# Auto message command permission")
     public String autoMessageCommandPermission = "command.automessage";
 
+    @Comment("# Notification configuration")
     public NotificationConfiguration notificationConfiguration = new NotificationConfiguration();
 
 }
